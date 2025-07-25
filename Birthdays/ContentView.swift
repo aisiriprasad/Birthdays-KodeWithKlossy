e45b99673709a75e3c60983eae987491f84a5c9e
Aisiri Prasad <aisiri.prasad@gmail.com>
2025-07-25 11:51:53 -0400

    Added code so users can update a friend: When the user taps on a friend’s name in the list, a new sheet will open, allowing the user to edit the friend’s name and birthday. From there, the user can either Save or Cancel the changes.

--- Birthdays/ContentView.swift
+++ Birthdays/ContentView.swift
@@ -12,9 +12,9 @@ struct ContentView: View {
     
     @Query private var friends:[Friend]
     @Environment(\.modelContext) private var context
-    
     @State private var newName = ""
     @State private var newBirthday = Date.now
+    @State private var selectedFriend: Friend?
     
     var body: some View {
         
@@ -26,12 +26,21 @@ struct ContentView: View {
                         Spacer()
                         Text(friend.birthday,format: .dateTime.month(.wide).day().year())
                     }
+                    .onTapGesture {
+                        selectedFriend = friend
+                    }
                 }
                 .onDelete(perform: deleteFriend)
             }
             
             .navigationTitle("Birthdays")
+            .sheet(item: $selectedFriend){friend in
+                NavigationStack{
+                    EditFriendView(friend: friend)
+                }
+            }
             .safeAreaInset(edge: .bottom) {
+                
                 VStack(alignment: .center,spacing: 20) {
                     Text("New Birthday")
                     .font(.headline)
